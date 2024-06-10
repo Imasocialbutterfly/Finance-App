cc5c4b3372aec67fd1d37ee21ed2413338d0989d
Frederick Butterfly <keitumetsemogotsif@gmail.com>
2024-06-10 19:36:19 +0200

    Accounts POST API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { useNewAccoount } from "@/features/accounts/hooks/use-new-account"
+
 export default function Home() {
+    const {onOpen} = useNewAccoount()
+
     return (
         <div>
-            Home page
+            <Button onClick={onOpen}>
+                Add and account
+            </Button>
         </div>
     )
 }
